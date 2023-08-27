8687e8e66a150cff26dc491e9f5e83e66cbd2332
francoonesto <francoonesto2001@gmail.com>
2023-08-27 17:44:47 -0300

    post

--- src/controllers/cartManager.js
+++ src/controllers/cartManager.js
@@ -1,3 +1,4 @@
+import { json } from 'express'
 import { promises as fs } from  'fs'
 import { v4 as uuidv4 } from 'uuid'
 
@@ -12,22 +13,19 @@ static aumentoId(){
     return uuidv4()
 }
 
-async addCart(prod) {
-    const products = JSON.parse(await fs.readFile(this.path , 'utf-8'))
-    const cart = products.find(c => c.productos === prod.productos)
+async crearCart(carrito) {
+    const cart = JSON.parse(await fs.readFile(this.path , 'utf-8'))
 
-    if(cart){
-        throw new Error("Producto ya existente")
+    if(true){
+        carrito.id = CartManager.aumentoId()
+        cart.push(carrito)
+        await fs.writeFile(this.path,JSON.stringify(cart))
     }
-    else{
-        prod.id = CartManager.aumentoId()
-        products.push(prod)
-        await fs.writeFile(this.path , JSON.stringify(products))
-    }}
+    }
 
-async cartById(id) {
-    const products = JSON.parse(await fs.readFile(this.path , 'utf-8'))
-    const existe = products.find(existe => existe.id === id)
+    async cartById(id) {
+    const cart = JSON.parse(await fs.readFile(this.path , 'utf-8'))
+    const existe = cart.find(existe => existe.id === id)
 
     if(!existe){
      throw new Error("Producto no encontrado")
@@ -35,8 +33,45 @@ async cartById(id) {
 
     return existe}
 
-// async cartProducts(prod , id) {
+async addProductToCart(prod){
+const cart = JSON.parse(await fs.readFile(this.path , 'utf-8'))
+const addCart = cart.find(add => add.id != id)
 
-// }
-
+if(addCart){
+    throw new Error("Producto no encontrado")
+} else{
+    prod.id = CartManager.aumentoId()
+    addCart.productos.push(prod)
+    const cantidad = productos.find(c => c.pid === prod.id)
+    if (cantidad){
+        return cart.reduce((acum , item ) => acum += item.cantidad + 1 , 0)
+    }
+    await fs.writeFile(this.path,JSON.stringify(cart))
 }
+}
+
+    }
+
+// async cartById(id) {
+//     const products = JSON.parse(await fs.readFile(this.path , 'utf-8'))
+//     const existe = products.find(existe => existe.id === id)
+
+//     if(!existe){
+//      throw new Error("Producto no encontrado")
+//     }
+
+//     return existe}
+
+    // async addCart(prod) {
+    //     const products = JSON.parse(await fs.readFile(this.path , 'utf-8'))
+    //     const cart = products.find(c => c.productos === prod.productos)
+
+    //     if(cart){
+    //         throw new Error("Producto ya existente")
+    //     }
+    //     else{
+    //         prod.id = CartManager.aumentoId()
+    //         products.push(prod)
+    //         await fs.writeFile(this.path , JSON.stringify(products))
+    //     }}
+
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -6,27 +6,25 @@ const cartManager = new CartManager('src/models/carrito.json')
 const routerCart = Router()
 
 routerCart.post('/' , async (req,res) =>{
-    try{
-        await cartManager.addCart(req.body)
-        res.status(200).send("Producto creado correctamente")
-    }
-    catch(error){res.status(400).send(error.message)}
-})
-
-// routerCart.post('/' , async (req,res) =>{
-//     try{
-//         await cartManager.addCart(req.body)
-//         res.status(200).send("Producto creado correctamente")
-//     }
-//     catch(error){res.status(400).send(error.message)}
-// })
-
+        await cartManager.crearCart(req.body)
+        res.status(200).send("cart creado")})
 routerCart.get('/:cid' , async (req,res) => {
     const {cid} = req.params
 
     try{
         const cart = await cartManager.cartById(cid)
         res.status(200).send(cart)
+    }
+    catch(error){res.status(400).send(error.message)}
+})
+routerCart.post('/:cid/product/:pid' , async (req,res) =>{
+const {cid} = req.params
+const {product} = req.body
+const {pid} = req.params
+
+    try{
+        await cartManager.addProductToCart(cid , product , pid)
+        res.status(200).send("Producto agregado correctamente")
     }
     catch(error){res.status(400).send(error.message)}
 })
